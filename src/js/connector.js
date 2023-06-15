f7e5c50e6ae79737bb4ff67b023c0059426d2bd2
badcodelab <id.vitech@gmail.com>
2023-06-15 17:06:51 +0300

    remove secons

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,8 +1,8 @@
 window.TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
         return t.card('due').get('due').then(function(due) {
-            return due && !/.+:59:59.000Z$/.test(due) ? [{
-                    text: new Date(due).toLocaleTimeString(),
+            return due && !/.+:59:(59|00).000Z$/.test(due) ? [{
+                    text: new Date(due).toLocaleTimeString([], { timeStyle: 'short' }),
                     color: 'light-gray',
                 }] : [];
         });
