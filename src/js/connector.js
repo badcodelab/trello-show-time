04c5cc649802a1923bf3dd13baf8ddb211212dee
badcodelab <id.vitech@gmail.com>
2023-06-15 16:55:41 +0300

    fix badge and show time

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,10 +1,8 @@
 window.TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
-        return t.card('all').then(function(card) {
-            console.log(card);
-
-            return card.due ? [{
-                    text: card.due.toLocaleTimeString(),
+        return t.card('due').get('due').then(function(due) {
+            return due && !/.+:59:59.000Z$/.test(due) ? [{
+                    text: new Date(due).toLocaleTimeString(),
                     color: 'light-gray',
                 }] : [];
         });
