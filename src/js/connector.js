d9cbde60697fa23eb0c8a3c99ffd75fce4358b9d
badcodelab <id.vitech@gmail.com>
2023-06-15 16:43:43 +0300

    show time badge

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,8 +1,12 @@
-console.log('showtime')
-
 window.TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
-        console.log(t, opts);
-        return [];
+        return t.card('all').then(function(card) {
+            console.log(card);
+
+            return card.due ? [{
+                    text: card.due.toLocaleTimeString(),
+                    color: 'light-gray',
+                }] : [];
+        });
     }
 });
